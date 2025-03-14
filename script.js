565722adfa4ecbb92761699e16aacf58c671ab29
rafadori <rafadori26@gmail.com>
2025-03-14 15:32:33 -0300

    feat(js): add blur transition

--- script.js
+++ script.js
@@ -17,3 +17,8 @@ window.addEventListener("DOMContentLoaded", () => {
     backgroundAlpha: 0.0,
   });
 });
+
+setTimeout(() => {
+  const main = document.querySelector("main");
+  main.style.opacity = 1;
+}, 1000);
