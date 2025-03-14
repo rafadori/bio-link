16d479dc8bab6ae09c8b6cb7977d781676239fb6
rafadori <rafadori26@gmail.com>
2025-03-14 15:04:12 -0300

    feat(js): add initial JavaScript functionality

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+  VANTA.BIRDS({
+    el: "#vanta",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    backgroundColor: 0xffffff,
+    color1: 0x344bb6,
+    color2: 0x643091,
+    quantity: 3.0,
+    backgroundAlpha: 0.0,
+  });
+});
